c7c94a42d269877774c6e83bc7d8fc714f5645db
maheshgit2 <maheshyadavm33@gmail.com>
2024-05-30 16:18:58 +0530

    EMS Frontend

---
+++ react-frontend/src/components/FooterComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import  '../App.css';
+
+function FooterComponent() {
+  return (
+    <div>
+          <footer className='footer'>
+            <span>All Right Reserved 2024 &copy; MaheshYadav</span>
+          </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
+
